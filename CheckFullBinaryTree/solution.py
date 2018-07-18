807afd7837746b1335474114d4566671f25e45d0
Rong Xie <rxie@juniper.net>
2018-07-17 21:51:04 -0700

    added Check Full Binary Tree

---
+++ CheckFullBinaryTree/solution.py
@@ -0,0 +1,27 @@
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+
+class Solution:
+    """
+    @param root: the given tree
+    @return: Whether it is a full tree
+    """
+    def isFullTree(self, root):
+        # write your code here
+        if root is None:
+            return True
+        if root.left is None and root.right is None:
+            return True
+        if root.left is None or root.right is None:
+            return False
+        
+        left = self.isFullTree(root.left)
+        right = self.isFullTree(root.right)
+        
+        return left and right
+
